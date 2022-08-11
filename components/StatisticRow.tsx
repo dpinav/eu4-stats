0338b09e4f1745addd86f5e5ff445097978c129c
David Pina <dpina@edu.tecnocampus.cat>
2022-08-11 21:56:57 +0200

    Attempt to fix NaN values

--- components/StatisticRow.tsx
+++ components/StatisticRow.tsx
@@ -42,16 +42,17 @@ const StatisticRow = (props: {
     (e: ICountryData) =>
       e.tag === countryData.tag || (e.player !== "undefined..." && e.player == countryData.player)
   );
-  const value: number = countryData[modifier.parameter as keyof ICountryData] as number;
+  let value: number = countryData[modifier.parameter as keyof ICountryData] as number;
   let lastValue: number = value;
   if (lastIndex >= 0) {
     lastValue = sortedLastCountriesData[lastIndex][
       modifier.parameter as keyof ICountryData
     ] as number;
   }
-  if (lastValue === 0) lastValue = value;
-  const change: number = value - lastValue;
-  const percentage: number = (change / lastValue) * 100;
+  if (lastValue === 0 || isNaN(lastValue)) lastValue = value;
+  let change: number = value - lastValue;
+  let percentage: number = (change / lastValue) * 100;
+
   return (
     <StyledTableRow
       key={countryData.tag}
@@ -97,7 +98,9 @@ const StatisticRow = (props: {
       </StyledTableCell>
       <StyledTableCell align="center">
         <TypographyCell>
-          {isFloat(value)
+          {!value
+            ? 0
+            : isFloat(value)
             ? parseFloat(value.toString()).toLocaleString("en-US") ?? 0
             : parseInt(value.toString()).toLocaleString("en-US")}
         </TypographyCell>
